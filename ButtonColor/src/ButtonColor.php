d74f702051abd90e7ab54549448e1346dd9af49f
Patrizio Marzullo <RealSkaarj@gmx.de>
2023-11-13 00:21:38 +0100

    first commit

---
+++ ButtonColor/src/ButtonColor.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace ButtonColor;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class ButtonColor extends Plugin implements ThemeInterface
+{
+}
---
+++ ButtonColor/src/Resources/app/storefront/dist/storefront/js/button-color.js
@@ -0,0 +1,1 @@
+(self.webpackChunk=self.webpackChunk||[]).push([["button-color"],{223:()=>{}},e=>{var k;k=223,e(e.s=k)}]);
